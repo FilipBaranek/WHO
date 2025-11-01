95eefc75d8cc35758f4e5507335642a5c5304290
FilipBaranek <filipbaranek22@gmail.com>
2025-11-01 20:47:08 +0100

    View operations predefined

--- src/Headers/View/UI/OperationsWindow.h
+++ src/Headers/View/UI/OperationsWindow.h
@@ -1,12 +1,38 @@
 #pragma once
+#include <string>
+#include <array>
 #include "Window.h"
 #include "OutputWindow.h"
 
 
 class OperationsWindow : public Window
 {
+private:
+	static constexpr int OPERATIONS_COUNT = 20;
+	std::array<std::function<void()>, OPERATIONS_COUNT> m_operations;
+
+	int m_firstNumInput = 0;
+	int m_secondNumInput = 0;
+	std::string m_firstStringBuf;
+	std::string m_secondStringBuf;
+
+	int m_currentItem = 0;
+
+
+	void displayTestIdPatientIdInputs();
+	void displayPatientIdInput();
+	void displayDistrictIdDateInputs();
+	void displayRegionIdDateInputs();
+	void displayDateInputs();
+	void displayDistrictIdDateSickDaysInput();
+	void displayRegionDateSickDaysInputs();
+	void displayDateSickDaysInputs();
+	void displayWorkplaceIdDateInputs();
+	void displayTestIdInputs();
+
+
 public:
-	OperationsWindow(Presenter* presenter) : Window(presenter) {}
+	OperationsWindow(Presenter* presenter);
 	void renderWindow() override;
 	~OperationsWindow() override = default;
 };
--- src/Source/View/UI/OperationsWindow.cpp
+++ src/Source/View/UI/OperationsWindow.cpp
@@ -1,5 +1,38 @@
 #include "../../../Headers/View/UI/OperationsWindow.h"
 
+
+OperationsWindow::OperationsWindow(Presenter* presenter) : Window(presenter)
+{
+    m_operations[0] = []() {};
+    m_operations[1] = [this]() { displayTestIdPatientIdInputs(); };
+}
+
+void OperationsWindow::displayTestIdPatientIdInputs()
+{
+    char birthBuf[20];
+    strncpy_s(birthBuf, sizeof(birthBuf), m_secondStringBuf.c_str(), _TRUNCATE);
+
+    float windowHeight = ImGui::GetWindowSize().y;
+    float rowHeight = ImGui::GetTextLineHeight() + ImGui::GetStyle().FramePadding.y * 2; // height of one input row
+    ImGui::SetCursorPosY((windowHeight - rowHeight) * 0.5f);
+
+    ImGui::Columns(2, nullptr, false);
+
+    ImGui::Text("Test ID:");
+    ImGui::InputInt("##testId", &m_firstNumInput, 0, 0);
+
+    ImGui::NextColumn();
+
+    ImGui::Text("Birth Number:");
+    if (ImGui::InputText("##birthNumber", birthBuf, IM_ARRAYSIZE(birthBuf)))
+    {
+        m_secondStringBuf = birthBuf;
+    }
+
+    ImGui::Columns(1);
+}
+
+
 void OperationsWindow::renderWindow()
 {
     ImGuiViewport* viewport = ImGui::GetMainViewport();
@@ -16,7 +49,6 @@ void OperationsWindow::renderWindow()
         ImGuiWindowFlags_NoSavedSettings
     );
 
-    static int current_item = 0;
     static const char* items[] = {
         "Select...",
         "2. Vysledok testu podla id testu a id pacienta",
@@ -25,19 +57,31 @@ void OperationsWindow::renderWindow()
         "5. Vsetky testy za dane casove odbobie pre okres",
         "6. Vsetky pozitivne testy za dane casove obdobie pre kraj",
         "7. Vsetky testy za dane casove obdobie pre kraj",
+        "8. Vsetky pozitivne testy za dane casove obdobie",
+        "9. Testy za dane casove obdobie",
+        "10. Chore osoby v okrese k zadanemu datumu",
+        "11. Usporiadane chore osoby k zadanemu datumu",
+        "12. Chore osoby v kraji k zadanemu datumu",
+        "13. Chore osoby k zadanemu datumu",
+        "14. Chora osoba k zadanemu datumu s najvyssou hodnotou testu v okrese",
+        "15. Okresy usporiadane podla poctu chorych osob",
+        "16. Kraje usporiadane podla poctu chorych osob",
+        "17. Testy uskutocnene za dane casove obdobie na zadanom pracovisku",
+        "18. Najdi test",
         "20. Odstranit test",
         "21. Odstranit osobu so vsetkymi zaznamami",
         "Vypisat vsetky udaje"
     };
 
-    if (ImGui::BeginCombo("Select Operation", items[current_item]))
+    if (ImGui::BeginCombo("Select Operation", items[m_currentItem]))
     {
         for (int n = 0; n < IM_ARRAYSIZE(items); n++)
         {
-            bool is_selected = (current_item == n);
+            bool is_selected = m_currentItem == n;
+
             if (ImGui::Selectable(items[n], is_selected))
             {
-                current_item = n;
+                m_currentItem = n;
             }
 
             if (is_selected)
@@ -46,6 +90,11 @@ void OperationsWindow::renderWindow()
             }
         }
         ImGui::EndCombo();
+    }
+
+    if (m_currentItem < m_operations.size())
+    {
+        m_operations[m_currentItem]();
     }
 
     float buttonWidth = 100.0f;
